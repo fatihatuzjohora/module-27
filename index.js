004b86356fa21729044215d1a132ce7904c4ce14
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-02-17 01:49:45 +0600

    part-1 finish

---
+++ index.js
@@ -0,0 +1,60 @@
+
+/*
+
+// section-1
+function play(){
+const homeSection=document.getElementById('homeScreen');
+
+homeSection.classList.add('hidden')
+
+// section-2
+const playGroundSection=document.getElementById('playGround')
+playGroundSection.classList.remove('hidden')
+}
+*/
+// others vbe 
+
+function hideElementById(elementId) {
+    const element=document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function play() {
+    hideElementById('homeScreen');
+    showElementById('playGround');
+    continueGame('')
+}
+
+function showElementById(elementId) {
+    const element=document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function continueGame () {
+    const alphabet=getARandomAlphabet();
+    console.log('rendom',alphabet);
+
+    // set bgcolor 
+     setBgColorById(alphabet);
+
+}
+
+function getARandomAlphabet() {
+    const alphabatesString='abcdefghijklmnopqrstuvwxyz';
+    const alphabets=alphabatesString.split('');
+   console.log(alphabets);
+    //math alphabets
+    const randomNumber= Math.random()*25;
+    const index=Math.round(randomNumber);
+    console.log(index);
+
+const alphabet=alphabets[index];
+ console.log(index,alphabet);
+return alphabet
+}
+
+// color add keybord click
+function setBgColorById(elementId) {
+    const element=document.getElementById(elementId);
+    element.classList.add('bg-orange-400');}
+
